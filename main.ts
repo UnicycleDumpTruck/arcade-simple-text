5d60d687d2ca3bdc3b0db66627eb660e0bfbdb89
Jeff Highsmith <junk@jeffhighsmith.com>
2020-08-01 15:52:31 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -17,3 +17,22 @@ namespace simpleText {
         return sprites.create(textImage, kind);
     }
 } 
+    
+namespace imagemorph {
+    //% blockId="spritemorphimage"
+    //% blockId=spritemorphimage block="morph %sprite(mySprite) image to %img=screen_image_picker"
+    //% weight=7 help=sprites/sprite/set-image
+    export function morph (mySprite: Sprite, myImage: Image) {
+        pixels_to_change = []
+        for (let index = 0; index <= 255; index++) {
+            pixels_to_change.push(index)
+        }
+        while (pixels_to_change.length > 0) {
+            random_pixel = pixels_to_change.removeAt(randint(0, pixels_to_change.length - 1))
+            row = Math.floor(random_pixel / 16)
+            column = random_pixel % 16
+            mySprite.image.setPixel(column, row, myImage.getPixel(column, row))
+            pause(0.01)
+        }
+    }
+}
