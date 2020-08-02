743fb8f67cfd6004258b082eb8baab3bd320998f
Jeff Highsmith <junk@jeffhighsmith.com>
2020-08-02 10:17:22 -0400

    Implemented variable rate. Added background morph.

--- main.ts
+++ main.ts
@@ -1,14 +1,15 @@
 namespace imagemorph {
-  export function transferPixel(mySprite: Sprite, myImage: Image, pixel_array: number[]) {
+  function transferPixel(destinationImage: Image, originImage: Image, pixel_array: number[]) {
       let random_pixel = pixel_array.removeAt(
         randint(0, pixel_array.length - 1)
       );
-      let y = Math.floor(random_pixel / myImage.width);
-      let x = random_pixel % myImage.width;
-      mySprite.image.setPixel(x, y, myImage.getPixel(x, y));
+      let y = Math.floor(random_pixel / originImage.width);
+      let x = random_pixel % originImage.width;
+      destinationImage.setPixel(x, y, originImage.getPixel(x, y));
     }
 
   
+  //% group="Sprites"
   //% blockId="spritemorphimage"
   //% blockId=spritemorphimage block="morph %sprite(mySprite) image to %img=screen_image_picker"
   //% weight=7 help=sprites/sprite/set-image
@@ -21,23 +22,34 @@ namespace imagemorph {
       pixels_to_change.push(index);
     }
     
-    
-    while (pixels_to_change.length > 100) {
-      let group_size = Math.floor(pixels_to_change.length/50)
+    while (pixels_to_change.length > 0) {
+      let group_size = Math.floor(pixels_to_change.length/100) + 1
       for (let index = 0; index < group_size; index++) {
-        transferPixel(mySprite, myImage, pixels_to_change);
+        transferPixel(mySprite.image, myImage, pixels_to_change);
       }
       pause(0.01);
     }
+  }
 
+  //% group="Screen"
+  //% blockId="backgroundmorphimage"
+  //% blockId=backgroundmorphimage block="morph background image to %img=background_image_picker"
+  //% weight=7
+  export function morphBackground(myImage: Image) {
+    const scene = game.currentScene();
+    let pixels_to_change = [];
+    let num_pixels = myImage.width * myImage.height;
+    for (let index = 0; index < num_pixels; index++) {
+      pixels_to_change.push(index);
+    }
+    
     while (pixels_to_change.length > 0) {
-      let random_pixel2 = pixels_to_change.removeAt(
-        randint(0, pixels_to_change.length - 1)
-      );
-      let y2 = Math.floor(random_pixel2 / myImage.width);
-      let x2 = random_pixel2 % myImage.width;
-      mySprite.image.setPixel(x2, y2, myImage.getPixel(x2, y2));
+      let group_size = Math.floor(pixels_to_change.length/100) + 1
+      for (let index = 0; index < group_size; index++) {
+        transferPixel(scene.background.image, myImage, pixels_to_change);
+      }
       pause(0.01);
     }
   }
+
 }
