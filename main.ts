a7d3255df3c4c40be349e7ac91264070ef6713c9
Jeff Highsmith <junk@jeffhighsmith.com>
2020-08-02 08:14:21 -0400

    new transferPixel function

--- main.ts
+++ main.ts
@@ -1,17 +1,32 @@
 //% blockNamespace=sprites color="#3B6FEA" blockGap=8
 namespace imagemorph {
+  export function transferPixel(mySprite: Sprite, myImage: Image, pixel_array: Array) {
+      let random_pixel = pixel_array.removeAt(
+        randint(0, pixel_array.length - 1)
+      );
+      let y = Math.floor(random_pixel / myImage.width);
+      let x = random_pixel % myImage.width;
+      mySprite.image.setPixel(x, y, myImage.getPixel(x, y));
+    }
+
+  
+  
   //% blockId="spritemorphimage"
   //% blockId=spritemorphimage block="morph %sprite(mySprite) image to %img=screen_image_picker"
   //% weight=7 help=sprites/sprite/set-image
   //% sprite.shadow="variables_get"
   //% sprite.defl="mySprite"
-
   export function morph(mySprite: Sprite, myImage: Image) {
     let pixels_to_change = [];
     let num_pixels = myImage.width * myImage.height;
     for (let index = 0; index < num_pixels; index++) {
       pixels_to_change.push(index);
     }
+    
+    while (pixels_to_change.length > 500) {
+      transfer_pixel(mySprite, myImage, pixels_to_change);
+    }
+
     while (pixels_to_change.length > 0) {
       let random_pixel = pixels_to_change.removeAt(
         randint(0, pixels_to_change.length - 1)
