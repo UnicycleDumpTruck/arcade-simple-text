5b09f7af663777e566d9cb4d57df638533cd728b
Jeff Highsmith <junk@jeffhighsmith.com>
2020-08-02 08:48:58 -0400

    Fixed error in rate usage.

--- main.ts
+++ main.ts
@@ -24,7 +24,7 @@ namespace imagemorph {
     
     while (pixels_to_change.length > 100) {
       let group_size = Math.floor(pixels_to_change.length/10)
-      for (let index = 0; index < 2; index++) {
+      for (let index = 0; index < group_size; index++) {
         transferPixel(mySprite, myImage, pixels_to_change);
       }
       pause(0.01);
